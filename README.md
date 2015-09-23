2185c5ffc0d529f5e9e4cb13af7671f1b4d9deba
Ryan Worth <ryan@edgeonline.com.au>
2015-09-23 15:58:53 +1000

    Fix error with size if not set or missing 'x'. Update README

--- README.md
+++ README.md
@@ -1,3 +1,63 @@
 # Jquery plugin for [PhotoSwipe](http://photoswipe.com)
 
+To use `jquery.photoswipe.js` include the following files in the `<head>` of your markup
+
+```
+	<script type='text/javascript' src='path/to/jquery.min.js'></script>
+    
+    <link rel='stylesheet' href='path/to/photoswipe.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='path/to/default-skin/default-skin.css' type='text/css' media='all' />
+    <script type='text/javascript' src='path/to/photoswipe.js'></script>
+    <script type='text/javascript' src='path/to/photoswipe-ui-default.js'></script>
+    
+    <script type='text/javascript' src='path/to/jquery.photoswipe.js'></script>
+
+```
+
+In your markup add a `rel` attribute with a value starting with `photoswipe-` to the `<a>` that you wish to be in the photoswipe gallery.
+The you can group links in a gallery by using the same suffix in your `rel` attribute. Make sure to set the `data-size` attirbute with the `[width]x[height]` of the image
+
+```
+    <h1>Gallery1</h1>
+    <div>
+            <a href="http://lorempixel.com/800/600/food/1/" data-size="800x600" data-caption="<p>Opis</p><small>opis mały</small>" rel="photoswipe-gallery1">
+                <img class="img-responsive" src="http://lorempixel.com/800/600/food/1/"/>
+            </a>
+            <a href="http://lorempixel.com/800/600/food/2/" data-size="800x600" rel="photoswipe-gallery1">
+                <img class="img-responsive" src="http://lorempixel.com/800/600/food/2/"/>
+            </a>
+            <a href="http://lorempixel.com/800/600/food/3/" data-size="800x600" rel="photoswipe-gallery1">
+                <img class="img-responsive" src="http://lorempixel.com/800/600/food/3/"/>
+            </a>
+            <a href="http://lorempixel.com/800/600/food/4/" data-size="800x600" rel="photoswipe-gallery1">
+                <img class="img-responsive" src="http://lorempixel.com/800/600/food/4/"/>
+            </a>
+    </div>
+    <h1>Gallery2</h1>
+    <div>
+            <a href="http://lorempixel.com/800/600/food/5/" data-size="800x600" rel="photoswipe-gallery2">
+                <img class="img-responsive" src="http://lorempixel.com/800/600/food/5/"/>
+            </a>
+            <a href="http://lorempixel.com/800/600/food/6/" data-size="800x600" rel="photoswipe-gallery2">
+                <img class="img-responsive" src="http://lorempixel.com/800/600/food/6/"/>
+            </a>
+            <a href="http://lorempixel.com/800/600/food/7/" data-size="800x600" rel="photoswipe-gallery2">
+                <img class="img-responsive" src="http://lorempixel.com/800/600/food/7/"/>
+            </a>
+            <a href="http://lorempixel.com/800/600/food/8/" data-size="800x600" rel="photoswipe-gallery2">
+                <img class="img-responsive" src="http://lorempixel.com/800/600/food/8/"/>
+            </a>
+    </div>
+
+```
+
+Then in your javascript select all anchors with the correctly set `rel` attribute and call the `photoswipe({options})` jQuery function
+
+```
+	$("a[rel^='photoswipe']").photoswipe();
+
+```
+
+# Example
+
 Once downloaded repository run `bower install` in the root directory to install dependencies for the example in `index.html`.
--- jquery.photoswipe.js
+++ jquery.photoswipe.js
@@ -5,18 +5,23 @@
         var galleries = [],
             _options = options,
             markup = '<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true"><div class="pswp__bg"></div><div class="pswp__scroll-wrap"><div class="pswp__container"><div class="pswp__item"></div><div class="pswp__item"></div><div class="pswp__item"></div></div><div class="pswp__ui pswp__ui--hidden"><div class="pswp__top-bar"><div class="pswp__counter"></div><button class="pswp__button pswp__button--close" title="Close (Esc)"></button><button class="pswp__button pswp__button--share" title="Share"></button><button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button><button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button><div class="pswp__preloader"><div class="pswp__preloader__icn"><div class="pswp__preloader__cut"><div class="pswp__preloader__donut"></div></div></div></div></div><div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap"><div class="pswp__share-tooltip"></div></div><button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button><button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button><div class="pswp__caption"><div class="pswp__caption__center"></div></div></div></div></div>';
-        $(markup).appendTo('body');
+        if($('.pswp').size() === 0) { $(markup).appendTo('body'); }
         
         var init = function($this){
             $this.each(function(){
                 var $anchor = $(this),
                     src = $anchor.attr('href'),
-                    size = $anchor.data('size').split('x'),
-                    caption = $anchor.data('caption');
-                    name = $anchor.attr('rel').split('-')[1],
+                    size = $anchor.data('size'),
+                    caption = $anchor.data('caption'),
+                    name = $anchor.attr('rel').substr($anchor.attr('rel').indexOf("-")),
                     itemExists = false;
-                if(size.length != 2){
-                    throw SyntaxError("Missing data-size attribute.");
+                if(typeof size != "undefined" && size.indexOf('x') >= 0){
+                    size = size.split('x');
+                    if(size.length != 2){
+                        throw SyntaxError('Attribute data-size is missing or is an incorrect format. E.g. data-size="[width]x[height]"');
+                    }
+                } else {
+                    throw SyntaxError('Attribute data-size is missing or is an incorrect format. E.g. data-size="[width]x[height]"');
                 }
                 $.each(galleries,function(key,elem){
                     if(elem.name === name){
@@ -37,7 +42,7 @@
                             h: parseInt(size[1],10),
                             title: caption,
                             el: $anchor.get(0)
-                        }
+                        };
                         $anchor.data('gallery-id',key+1);
                         $anchor.data('photo-id',elem.items.length);
                         elem.items.push(item);
@@ -50,7 +55,7 @@
                     openGallery(gid,pid);
                 });
             });
-        }
+        };
         
         var parseHash = function() {
             var hash = window.location.hash.substring(1),
@@ -65,10 +70,10 @@
                 if(!vars[i]) {
                     continue;
                 }
-                var pair = vars[i].split('=');  
+                var pair = vars[i].split('=');
                 if(pair.length < 2) {
                     continue;
-                }           
+                }
                 params[pair[0]] = pair[1];
             }
 
@@ -92,7 +97,7 @@
                     getThumbBoundsFn: function(index) {
                         var thumbnail = items[index].el.children[0],
                             pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
-                            rect = thumbnail.getBoundingClientRect(); 
+                            rect = thumbnail.getBoundingClientRect();
 
                         return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
                     }
@@ -100,7 +105,7 @@
             $.extend(options,_options);
             var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
             gallery.init();
-        }
+        };
         
         // initialize
         init(this);
